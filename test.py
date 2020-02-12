d8e683957bf4ca5bbe13aceaaa0cc57e44a9b362
Gianni De Fabritiis <g.defabritiis@gmail.com>
2020-02-12 15:50:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ atom_vel = torch.zeros(natoms, 3).to(device)
 box = torch.zeros((1, 3)).to(device)
 
 atom_types = ["a", "a", "b", "c", "a", "b"]
-bonds = [[0, 1], [0, 2], [1, 3], [2, 4], [3, 4], [4, 5]]
+bonds = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]
 ev = Evaluator("forcefield.yaml", atom_types, bonds, device=device)
 
 #pot, atom_force = ev.evaluateEnergiesForces(atom_pos, box)
