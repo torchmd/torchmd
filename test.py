922f68079b7d31e100a607ab819844fe24236be3
Stefan Doerr <stefdoerr@gmail.com>
2020-02-12 18:30:05 +0200

    return trajectory of simulation

--- test.py
+++ test.py
@@ -31,3 +31,24 @@ ev = Evaluator("tests/water/water_forcefield.yaml", atom_types, bonds, device=de
 
 atom_mass = ev.masses[:, None]
 velocityverlet(atom_pos, atom_mass, ev, 100, box, energies=("LJ", "Bonds"), device=device)
+
+
+### Argon test
+from moleculekit.molecule import Molecule
+
+device = "cuda"
+mol = Molecule("./tests/argon/argon_start.pdb")
+mol.atomtype[:] = "AR"
+atom_pos = torch.tensor(mol.coords[:, :, 0].squeeze()).to(device)
+box = mol.coords.max(axis=0) - mol.coords.min(axis=0)
+box = torch.tensor(box.squeeze()).to(device)
+atom_types = mol.atomtype
+bonds = mol.bonds.astype(int).copy()
+
+ev = Evaluator("tests/argon/argon_forcefield.yaml", atom_types, bonds, device=device)
+
+atom_mass = ev.masses[:, None]
+traj = velocityverlet(atom_pos, atom_mass, ev, 10000, box, energies=("LJ"), device=device, timestep=10)
+
+mol.coords = traj
+mol.view()
--- torchmd/velocityverlet.py
+++ torchmd/velocityverlet.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 TIMEFACTOR = 48.88821
 BOLTZMAN = 0.001987191
@@ -21,7 +22,7 @@ def second_VV(vel, force, mass, dt):
     Ekin = 0.5 * torch.sum(torch.sum(vel * vel, dim=1) * mass)
     return Ekin
 
-def velocityverlet(pos, mass, ev, niter, box, energies=("LJ", "Bonds"), device="cpu", externalCalc=(),timestep=1):
+def velocityverlet(pos, mass, ev, niter, box, energies=("LJ", "Bonds"), device="cpu", externalCalc=(), timestep=1):
     if not (isinstance(externalCalc, list) or isinstance(externalCalc, tuple)):
         externalCalc = [externalCalc,]
         
@@ -41,6 +42,7 @@ def velocityverlet(pos, mass, ev, niter, box, energies=("LJ", "Bonds"), device="
     print(f"{'Step':<10} {'Epot':<10} {'Ekin':<10} {'Etotal':<10} {'T':<10}")
     print(f"{'0':<10} {Epot:<10.4f} {Ekin:<10.4f} {Epot+Ekin:<10.4f} {' ':<10}")
 
+    traj = []
     for n in range(niter):
         first_VV(pos, vel, force, mass, dt)
         force.zero_()
@@ -54,3 +56,5 @@ def velocityverlet(pos, mass, ev, niter, box, energies=("LJ", "Bonds"), device="
         Ekin = second_VV(vel, force, mass, dt)
         T = kinetic_to_temp(Ekin, natoms)
         print(f"{n:<10d} {Epot:<10.4f} {Ekin:<10.4f} {Epot+Ekin:<10.4f} {T:<10.4f}")
+        traj.append(pos.cpu().numpy())
+    return np.stack(traj, axis=2)
