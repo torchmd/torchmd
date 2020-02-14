b068e8e8fcae35edc24566f36662452f63a4002f
Stefan Doerr <stefdoerr@gmail.com>
2020-02-14 15:23:34 +0200

    prettier VV code

--- torchmd/velocityverlet.py
+++ torchmd/velocityverlet.py
@@ -10,19 +10,13 @@ def kinetic_to_temp(Ekin, nfree):
     return 2.0 * Ekin / (3.0 * nfree * BOLTZMAN)
 
 def first_VV(pos, vel, force, mass, dt):
-    imass = 1.0 / mass
-    coeff = 0.5 * dt * dt * imass
-    pos += vel * dt + coeff * force
-    coeff = 0.5 * dt * imass
-    vel += coeff * force
+    accel = force / mass
+    pos += vel * dt + 0.5 * accel * dt * dt 
+    vel += 0.5 * dt * accel
 
 def second_VV(vel, force, mass, dt):
-    imass = 1.0 / mass
-    Ekin = 0
-    coeff = 0.5 * dt * imass
-    vel += coeff * force
-    Ekin = 0.5 * torch.sum(torch.sum(vel * vel, dim=1) * mass)
-    return Ekin
+    accel = force / mass
+    vel += 0.5 * dt * accel
 
 def calculateMoleculeGroups(natoms, bonds, device):
     import networkx as nx
@@ -85,7 +79,8 @@ def velocityverlet(pos, mass, ev, niter, box, energies=("LJ", "Bonds"), device="
             EPot += ext_ene
             force += ext_force
 
-        Ekin = second_VV(vel, force, mass, dt)
+        second_VV(vel, force, mass, dt)
+        Ekin = 0.5 * torch.sum(torch.sum(vel * vel, dim=1) * mass)
         T = kinetic_to_temp(Ekin, natoms)
         if n % trajfreq == 0:
             tqdm_iter.write(f"{n:<10d} {Epot:<10.4f} {Ekin:<10.4f} {Epot+Ekin:<10.4f} {T:<10.4f}")
