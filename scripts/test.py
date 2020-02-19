2effafc7fb4405c216bbee9ae91ffb8c9a229edc
Gianni De Fabritiis <g.defabritiis@gmail.com>
2020-02-19 12:39:39 +0100

    WIP

--- scripts/test.py
+++ scripts/test.py
@@ -3,6 +3,10 @@ from torchmd.systems import Systems
 
 from moleculekit.molecule import Molecule
 
+from torchmd.forcefield import Forcefield
+from torchmd.forces import Forces
+from torchmd.integrator import Integrator
+
 device = "cpu"
 mol = Molecule("./tests/argon/argon_start.pdb")
 mol.atomtype[:] = "AR"
@@ -14,4 +18,12 @@ atom_types = mol.atomtype
 atom_mass = ev.masses[:, None]
 atom_vel = torch.zeros()
 
-systems  = Systems(atom_pos,vel,atom_mass)
+systems  = Systems(atom_pos,vel)
+
+
+forcefield = Forcefield("tests/argon/argon_forcefield.yaml",device)
+parameters = forcefield.create(atom_types,bonds=None)
+forces = Forces(parameters,device)
+integrator = Integrator(timestep)
+
+Ekin,Epot,T = integrator.step(niter=10)
