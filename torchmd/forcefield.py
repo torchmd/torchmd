2b97735df0089c022ca436d6e4d7e41488fd591a
Gianni De Fabritiis <g.defabritiis@gmail.com>
2020-02-19 12:29:59 +0100

    WIP

--- torchmd/forcefield.py
+++ torchmd/forcefield.py
@@ -2,19 +2,19 @@ import numpy as np
 import torch
 import yaml
 
-def calculateAB(sigma, epsilon):
-    # Lorentz - Berthelot combination rule
-    sigma_table = 0.5 * (sigma + sigma[:, None])
-    eps_table = np.sqrt(epsilon * epsilon[:, None])
-    sigma_table_6 = sigma_table ** 6
-    sigma_table_12 = sigma_table_6 * sigma_table_6
-    A = eps_table * 4 * sigma_table_12
-    B = eps_table * 4 * sigma_table_6
-    del sigma_table_12, sigma_table_6, eps_table, sigma_table
-    return A, B
-
+class Parameters:
+    A=None
+    B=None
+    bonds=None
+    bond_params=None
+    charges = None
+    masses =None
+    mapped_atom_types=None
 
 class Forcefield:
+    '''
+    This class takes as input the yaml forcefield file, the atom_types and outputs the system Parameters
+    '''
     def __init__(self,filename,device):
         self.ff = yaml.load(open(filename), Loader=yaml.FullLoader)
         self.device = device
@@ -23,13 +23,13 @@ class Forcefield:
         atomtype_map = {}
         for i, at in enumerate(self.ff["atomtypes"]):
             atomtype_map[at] = i
-        self.mapped_atom_types = torch.tensor([atomtype_map[at] for at in atom_types])
-        natoms = len(atom_types)
-        A,B=self.make_lj(atomtype_map)
-        ava_idx = self.make_indeces(natoms) #move
-        charges = self.make_charges(atom_types)
-        bond_params,bonds = self.make_bonds(bonds,atom_types)
-        masses = self.make_masses(atom_types)
+        par = Parameters()
+        par.mapped_atom_types = torch.tensor([atomtype_map[at] for at in atom_types]) 
+        par.A,par.B=self.make_lj(atomtype_map)
+        par.charges = self.make_charges(atom_types)
+        par.bond_params,par.bonds = self.make_bonds(bonds,atom_types)
+        par.masses = self.make_masses(atom_types)
+        return par
 
     def make_lj(self,atomtype_map):
         sorted_keys = [x[0] for x in sorted(atomtype_map.items(), key=lambda item: item[1])]
@@ -40,13 +40,6 @@ class Forcefield:
         B = torch.tensor(B).to(self.device)
         return A,B
 
-    def make_indeces(self,natoms):
-        allvsall_indeces = []
-        for i in range(natoms):
-            for j in range(i + 1, natoms):
-                allvsall_indeces.append([i, j])
-        ava_idx = torch.tensor(allvsall_indeces).to(self.device)
-        return ava_idx
         
     def make_charges(self,atom_types):
         return torch.tensor([self.ff["electrostatics"][at]["charge"] for at in atom_types]).to(self.device)
@@ -73,3 +66,15 @@ class Forcefield:
 
     def make_masses(self,atom_types):
         return torch.tensor([self.ff["masses"][at] for at in atom_types]).to(self.device)
+
+
+def calculateAB(sigma, epsilon):
+    # Lorentz - Berthelot combination rule
+    sigma_table = 0.5 * (sigma + sigma[:, None])
+    eps_table = np.sqrt(epsilon * epsilon[:, None])
+    sigma_table_6 = sigma_table ** 6
+    sigma_table_12 = sigma_table_6 * sigma_table_6
+    A = eps_table * 4 * sigma_table_12
+    B = eps_table * 4 * sigma_table_6
+    del sigma_table_12, sigma_table_6, eps_table, sigma_table
+    return A, B
---
+++ torchmd/forces.py
@@ -0,0 +1,124 @@
+from scipy import constants as const
+import torch
+from torchmd.util import calculateDistances
+import yaml
+import numpy as np
+
+from forcefield import Parameters
+
+class Forces:
+    nonbonded = ["Electrostatics","LJ","Repulsion","repulsionCG"]
+
+    def __init__(self, parameters):
+        self.par = parameters
+        natoms = len()
+        self.ava_idx = self._make_indeces(natoms)
+
+    def compute(self, pos, forces, energies=("Bonds"), box=None):
+        pot = 0
+        #systems.zero_force()
+        if any(f in self.nonbonded for f in energies):
+            # Lazy mode: Do all vs all distances
+            dist, direction_unitvec = calculateDistances(pos, self.ava_idx[:, 0], self.ava_idx[:, 1], box)
+
+        for v in energies:
+            if v=="Bonds":
+                dist, direction_unitvec = calculateDistances(pos, self.par.bonds[:, 0], self.par.bonds[:, 1], box)
+                E, force_coeff = evaluateBonds(dist, self.par.bond_params)
+                pairs = self.par.bonds
+
+            if v=="Electrostatics":
+                E, force_coeff = evaluateElectrostatics(dist, self.ava_idx, self.par.charges)
+                pairs = self.ava_idx
+
+            if v=="LJ":
+                E, force_coeff = evaluateLJ(dist, self.ava_idx, self.par.mapped_atom_types, self.par.A, self.par.B)
+                pairs = self.ava_idx
+
+            if v=="Repulsion":
+                E, force_coeff = evaluateRepulsion(dist, self.ava_idx, self.mapped_atom_types, self.A)
+                pairs = self.ava_idx
+                
+            if v=="repulsionCG":
+                E, force_coeff = evaluateRepulsionCG(dist, self.ava_idx, self.mapped_atom_types, self.B)
+                pairs = self.ava_idx
+
+            pot += E.sum()
+            force[pairs[:, 0]] += (direction_unitvec * force_coeff[:, None])
+            force[pairs[:, 1]] -= (direction_unitvec * force_coeff[:, None])
+
+        return pot, force
+
+    def _make_indeces(self,natoms):
+        allvsall_indeces = []
+        for i in range(natoms):
+            for j in range(i + 1, natoms):
+                allvsall_indeces.append([i, j])
+        ava_idx = torch.tensor(allvsall_indeces).to(self.device)
+        return ava_idx
+
+
+
+ELEC_FACTOR = 1 / (4 * const.pi * const.epsilon_0)  # Coulomb's constant
+ELEC_FACTOR *= const.elementary_charge ** 2  # Convert elementary charges to Coulombs
+ELEC_FACTOR /= const.angstrom  # Convert Angstroms to meters
+ELEC_FACTOR *= const.Avogadro / (const.kilo * const.calorie)  # Convert J to kcal/mol
+
+
+def evaluateLJ(dist, pair_indeces, atom_types, A, B, scale=1):
+    atomtype_indices = atom_types[pair_indeces]
+    aa = A[atomtype_indices[:, 0], atomtype_indices[:, 1]]
+    bb = B[atomtype_indices[:, 0], atomtype_indices[:, 1]]
+
+    rinv1 = 1 / dist
+    rinv6 = rinv1 ** 6
+    rinv12 = rinv6 * rinv6
+
+    pot = ((aa * rinv12) - (bb * rinv6)) / scale
+    force = (-12 * aa * rinv12 + 6 * bb * rinv6) * rinv1 / scale
+    return pot, force
+
+
+def evaluateRepulsion(dist, pair_indeces, atom_types, A, scale=1):  # LJ without B
+    atomtype_indices = atom_types[pair_indeces]
+    aa = A[atomtype_indices[:, 0], atomtype_indices[:, 1]]
+
+    rinv1 = 1 / dist
+    rinv6 = rinv1 ** 6
+    rinv12 = rinv6 * rinv6
+
+    pot = (aa * rinv12) / scale
+    force = (-12 * aa * rinv12) * rinv1 / scale
+    return pot, force
+
+def evaluateRepulsionCG(dist, pair_indeces, atom_types, B, scale=1):  # Repulsion like from CGNet 
+    atomtype_indices = atom_types[pair_indeces]
+    bb = B[atomtype_indices[:, 0], atomtype_indices[:, 1]]
+
+    rinv1 = 1 / dist
+    rinv6 = rinv1 ** 6
+
+    pot = (bb * rinv6) / scale
+    force = (-6 * bb * rinv6) * rinv1 / scale
+    return pot, force
+
+
+def evaluateElectrostatics(dist, pair_indeces, atom_charges, scale=1):
+    pot = (
+        ELEC_FACTOR
+        * atom_charges[pair_indeces[:, 0]]
+        * atom_charges[pair_indeces[:, 1]]
+        / dist
+        / scale
+    )
+    force = -pot / dist
+    return pot, force
+
+
+def evaluateBonds(dist, bond_params):
+    k0 = bond_params[:, 0]
+    d0 = bond_params[:, 1]
+    x = dist - d0
+    pot = k0 * (x ** 2)
+    force = 2 * k0 * x
+    return pot, force
