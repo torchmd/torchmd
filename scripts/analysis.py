e574a3cae9e7fbf09c32429461890e8992cb0e69
Gianni De Fabritiis <g.defabritiis@gmail.com>
2020-02-25 05:10:39 +0100

    add simple analysis script

---
+++ scripts/analysis.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+import numpy as np
+from moleculekit.molecule import Molecule
+
+structure='./tests/argon/argon_start.pdb'
+df = pd.read_csv('mytest/monitor.csv')
+df.plot(x='ns',y='etot')
+
+mol=Molecule(structure)
+coords = np.load('mytest/output.npy')
+mol.coords=coords
+mol.reps.add('all','vdw')
+mol.view()
