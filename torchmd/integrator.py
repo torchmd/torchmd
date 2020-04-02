8ce6d335716b5f51d328077a83aed4cf887e6fa3
Stefan Doerr <stefdoerr@gmail.com>
2020-04-02 13:36:33 +0300

    blacken

--- torchmd/integrator.py
+++ torchmd/integrator.py
@@ -1,23 +1,26 @@
-
 import torch
 
 TIMEFACTOR = 48.88821
 BOLTZMAN = 0.001987191
 
-def kinetic_energy(masses,vel):
-    Ekin = torch.sum(0.5 * torch.sum(vel * vel, dim=1,keepdim=True) * masses)
+
+def kinetic_energy(masses, vel):
+    Ekin = torch.sum(0.5 * torch.sum(vel * vel, dim=1, keepdim=True) * masses)
     return Ekin
 
-def maxwell_boltzmann(masses,T, replicas=1):
+
+def maxwell_boltzmann(masses, T, replicas=1):
     natoms = len(masses)
     velocities = []
     for i in range(replicas):
-        velocities.append(torch.sqrt(T*BOLTZMAN/masses)*torch.randn((natoms,3)))
+        velocities.append(torch.sqrt(T * BOLTZMAN / masses) * torch.randn((natoms, 3)))
 
     return torch.stack(velocities, dim=0)
 
+
 def kinetic_to_temp(Ekin, natoms):
-    return 2.0/(3.0 * natoms * BOLTZMAN) * Ekin 
+    return 2.0 / (3.0 * natoms * BOLTZMAN) * Ekin
+
 
 def _first_VV(pos, vel, force, mass, dt):
     accel = force / mass
@@ -30,35 +33,40 @@ def _second_VV(vel, force, mass, dt):
     vel += 0.5 * dt * accel
 
 
-def langevin(vel,gamma,coeff,dt,device):
-    csi = torch.randn_like(vel).to(device)*coeff
-    vel += -gamma*vel*dt + csi 
+def langevin(vel, gamma, coeff, dt, device):
+    csi = torch.randn_like(vel).to(device) * coeff
+    vel += -gamma * vel * dt + csi
 
-PICOSEC2TIMEU=1000.0/TIMEFACTOR
+
+PICOSEC2TIMEU = 1000.0 / TIMEFACTOR
+
+
 class Integrator:
-    def __init__(self,systems,forces,timestep,device,gamma=None,T=None):
+    def __init__(self, systems, forces, timestep, device, gamma=None, T=None):
         self.dt = timestep / TIMEFACTOR
         self.systems = systems
         self.forces = forces
         self.device = device
-        gamma = gamma/PICOSEC2TIMEU
+        gamma = gamma / PICOSEC2TIMEU
         self.gamma = gamma
         self.T = T
         if T:
-            M=self.forces.par.masses
-            self.vcoeff=torch.sqrt(2.0*gamma/M*BOLTZMAN*T*self.dt).to(device)
-
+            M = self.forces.par.masses
+            self.vcoeff = torch.sqrt(2.0 * gamma / M * BOLTZMAN * T * self.dt).to(
+                device
+            )
 
     def step(self, niter=1):
         s = self.systems
         masses = self.forces.par.masses
         natoms = len(masses)
         for _ in range(niter):
-            _first_VV(s.pos,s.vel,self.forces.forces, masses,self.dt)
-            pot = self.forces.compute(s.pos,s.box)
-            if self.T: langevin(s.vel,self.gamma,self.vcoeff,self.dt,self.device)
+            _first_VV(s.pos, s.vel, self.forces.forces, masses, self.dt)
+            pot = self.forces.compute(s.pos, s.box)
+            if self.T:
+                langevin(s.vel, self.gamma, self.vcoeff, self.dt, self.device)
             _second_VV(s.vel, self.forces.forces, masses, self.dt)
 
-        Ekin = kinetic_energy(masses,s.vel).item()
+        Ekin = kinetic_energy(masses, s.vel).item()
         T = kinetic_to_temp(Ekin, natoms)
-        return Ekin,pot,T
+        return Ekin, pot, T
