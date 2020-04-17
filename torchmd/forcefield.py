5199846b18227fc017c088ab0a895cc383e647be
Stefan Doerr <stefdoerr@gmail.com>
2020-04-17 12:58:38 +0300

    support for X terms

--- torchmd/forcefield.py
+++ torchmd/forcefield.py
@@ -49,34 +49,66 @@ class Forcefield:
             par.angle_params, par.angles = self.make_angles(angles,atom_types)
         return par
 
+    def _get_x_variants(self, atomtypes):
+        from itertools import product
+
+        permutations = np.array(sorted(list(product([False, True], repeat=len(atomtypes))), key=lambda x: sum(x)))
+        variants = []
+        for per in permutations:
+            tmpat = atomtypes.copy()
+            tmpat[per] = 'X'
+            variants.append(tmpat)
+        return variants
+
+    def get_parameters(self, term, atomtypes):
+        atomtypes = np.array(atomtypes)
+        variants = self._get_x_variants(atomtypes)
+        if term == 'bonds' or term == 'angles' or term == 'dihedrals':
+            variants += self._get_x_variants(atomtypes[::-1])
+        elif term == 'impropers':
+            # TODO: need to handle the shitty sorting of impropers
+            pass
+        variants = sorted(variants, key=lambda x: sum(x == 'X'))
+        
+        termpar = self.ff[term]
+        for var in variants:
+            atomtypestr = ', '.join(var)
+            if len(var) > 1:
+                atomtypestr = '(' + atomtypestr + ')'
+            if atomtypestr in termpar:
+                return termpar[atomtypestr]
+        raise RuntimeError(f"{atomtypes} doesn't have {term} information in the FF")
+            
+
     def make_lj(self,atomtype_map):
         sorted_keys = [x[0] for x in sorted(atomtype_map.items(), key=lambda item: item[1])]
-        sigma = np.array([self.ff["lj"][at]["sigma"] for at in sorted_keys], dtype=np.float64)
-        epsilon = np.array([self.ff["lj"][at]["epsilon"] for at in sorted_keys], dtype=np.float64)
+        sigma = []
+        epsilon = []
+        for at in sorted_keys:
+            par = self.get_parameters('lj', [at,])
+            sigma.append(par["sigma"])
+            epsilon.append(par["epsilon"])
+        sigma = np.array(sigma, dtype=np.float64)
+        epsilon = np.array(epsilon, dtype=np.float64)
+
         A, B = calculateAB(sigma, epsilon)
         A = torch.tensor(A).type(self.precision)
         B = torch.tensor(B).type(self.precision)
         return A,B
         
-        
     def make_charges(self,atom_types):
-        return torch.tensor([self.ff["electrostatics"][at]["charge"] for at in atom_types])
+        charges = []
+        for at in atom_types:
+            par = self.get_parameters('electrostatics', [at,])
+            charges.append(par["charge"])
+        return torch.tensor(charges)
 
     def make_bonds(self, bonds, atom_types):
         bond_params = []
         uqbonds = set([tuple(sorted(pair)) for pair in bonds])
         uqbonds = [sorted(list(pair)) for pair in uqbonds]
         for pair in uqbonds:
-            pair_atomtype = f"({atom_types[pair[0]]}, {atom_types[pair[1]]})"
-            inv_pair_atomtype = f"({atom_types[pair[1]]}, {atom_types[pair[0]]})"
-            if pair_atomtype in self.ff["bonds"]:
-                bp = self.ff["bonds"][pair_atomtype]
-            elif inv_pair_atomtype in self.ff["bonds"]:
-                bp = self.ff["bonds"][inv_pair_atomtype]
-            else:
-                raise RuntimeError(
-                    f"{pair_atomtype} doesn't have bond information in the FF"
-                )
+            bp = self.get_parameters("bonds", [atom_types[pair[0]], atom_types[pair[1]]])
             bond_params.append([bp["k0"], bp["req"]])
         bond_params = torch.tensor(bond_params).type(self.precision)
         bonds = torch.tensor(uqbonds)
@@ -92,16 +124,7 @@ class Forcefield:
             uqangles.append(aa)
 
         for triplet in uqangles:
-            triplet_atomtype = f"({atom_types[triplet[0]]}, {atom_types[triplet[1]]}, {atom_types[triplet[2]]})"
-            inv_triplet_atomtype = f"({atom_types[triplet[2]]}, {atom_types[triplet[1]]}, {atom_types[triplet[0]]})"
-            if triplet_atomtype in self.ff["angles"]:
-                ap = self.ff["angles"][triplet_atomtype]
-            elif inv_triplet_atomtype in self.ff["angles"]:
-                ap = self.ff["angles"][inv_triplet_atomtype]
-            else:
-                raise RuntimeError(
-                    f"{pair_atomtype} doesn't have bond information in the FF"
-                )
+            ap = self.get_parameters("angles", [atom_types[triplet[0]], atom_types[triplet[1]], atom_types[triplet[2]]])
             angle_params.append([ap["k0"], np.deg2rad(ap["theta0"])])
         angle_params = torch.tensor(angle_params).type(self.precision)
         angles = torch.tensor(uqangles)
