506c4db5fc3b489a284803840bcc98dbd3913ad1
Stefan Doerr <stefdoerr@gmail.com>
2020-05-20 13:30:00 +0300

    black

--- torchmd/utils.py
+++ torchmd/utils.py
@@ -11,22 +11,22 @@ import numpy as np
 
 
 class LogWriter(object):
-    #kind of inspired form openai.baselines.bench.monitor
-    #We can add here an optional Tensorboard logger as well
-    def __init__(self, path, keys, header='', name='monitor.csv'):
-        self.keys = tuple(keys)+('t',)
+    # kind of inspired form openai.baselines.bench.monitor
+    # We can add here an optional Tensorboard logger as well
+    def __init__(self, path, keys, header="", name="monitor.csv"):
+        self.keys = tuple(keys) + ("t",)
         assert path is not None
-        
+
         os.makedirs(path, exist_ok=True)
         filename = os.path.join(path, name)
         if os.path.exists(filename):
             os.remove(filename)
 
-        print("Writing logs to ",filename)
+        print("Writing logs to ", filename)
 
         self.f = open(filename, "wt")
         if isinstance(header, dict):
-            header = '# {} \n'.format(json.dumps(header))
+            header = "# {} \n".format(json.dumps(header))
         self.f.write(header)
         self.logger = csv.DictWriter(self.f, fieldnames=self.keys)
         self.logger.writeheader()
@@ -36,41 +36,42 @@ class LogWriter(object):
     def write_row(self, epinfo):
         if self.logger:
             t = time.time() - self.tstart
-            epinfo['t'] = t
+            epinfo["t"] = t
             self.logger.writerow(epinfo)
             self.f.flush()
 
+
 class LoadFromFile(argparse.Action):
-#parser.add_argument('--file', type=open, action=LoadFromFile)
-    def __call__ (self, parser, namespace, values, option_string = None):
+    # parser.add_argument('--file', type=open, action=LoadFromFile)
+    def __call__(self, parser, namespace, values, option_string=None):
         if values.name.endswith("yaml") or values.name.endswith("yml"):
             with values as f:
                 namespace.__dict__.update(yaml.load(f, Loader=yaml.FullLoader))
                 return
 
         with values as f:
-            input=f.read()
-            input=input.rstrip()
-            for lines in input.split('\n'):
-                k,v=lines.split('=')
-                typ=type(namespace.__dict__[k])
-                v= typ(v) if typ is not None else v
-                namespace.__dict__[k]=v
+            input = f.read()
+            input = input.rstrip()
+            for lines in input.split("\n"):
+                k, v = lines.split("=")
+                typ = type(namespace.__dict__[k])
+                v = typ(v) if typ is not None else v
+                namespace.__dict__[k] = v
 
 
-def save_argparse(args,filename,exclude=None):
-    if filename.endswith('yaml') or filename.endswith('yml'):
+def save_argparse(args, filename, exclude=None):
+    if filename.endswith("yaml") or filename.endswith("yml"):
         if isinstance(exclude, str):
-            exclude = [exclude,]
+            exclude = [
+                exclude,
+            ]
         args = args.__dict__.copy()
         for exl in exclude:
             del args[exl]
-        yaml.dump(args, open(filename, 'w'))
+        yaml.dump(args, open(filename, "w"))
     else:
-        with open(filename, 'w') as f:
-            for k,v in args.__dict__.items():
-                if k is exclude: continue
-                f.write(f'{k}={v}\n')
-
-
-
+        with open(filename, "w") as f:
+            for k, v in args.__dict__.items():
+                if k is exclude:
+                    continue
+                f.write(f"{k}={v}\n")
