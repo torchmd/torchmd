c015f075d292822f680a85e7a70fe83a41b51710
AntonioMirarchi <antonio.mirarchi@upf.edu>
2022-12-05 14:54:10 +0100

    update if check on terms list for LJ

--- torchmd/parameters.py
+++ torchmd/parameters.py
@@ -32,8 +32,10 @@ class Parameters:
         self.to_(device)
 
     def to_(self, device):
-        self.A = self.A.to(device)
-        self.B = self.B.to(device)
+        if self.A is not None:
+            self.A = self.A.to(device)
+        if self.B is not None:
+            self.B = self.B.to(device)
         self.charges = self.charges.to(device)
         self.masses = self.masses.to(device)
         if self.bonds is not None:
@@ -59,8 +61,10 @@ class Parameters:
         self.device = device
 
     def precision_(self, precision):
-        self.A = self.A.type(precision)
-        self.B = self.B.type(precision)
+        if self.A is not None:
+            self.A = self.A.type(precision)
+        if self.B is not None:
+            self.B = self.B.type(precision)
         self.charges = self.charges.type(precision)
         self.masses = self.masses.type(precision)
         if self.bonds is not None:
@@ -103,7 +107,8 @@ class Parameters:
         self.mapped_atom_types = torch.tensor(indexes)
         self.charges = torch.tensor(mol.charge.astype(np.float64))
         self.masses = self.make_masses(ff, mol.atomtype)
-        self.A, self.B = self.make_lj(ff, uqatomtypes)
+        if "lj" in terms:
+            self.A, self.B = self.make_lj(ff, uqatomtypes)
         if "bonds" in terms and len(mol.bonds):
             uqbonds = np.unique([sorted(bb) for bb in mol.bonds], axis=0)
             self.bonds = torch.tensor(uqbonds.astype(np.int64))
@@ -164,7 +169,7 @@ class Parameters:
         A = torch.tensor(A)
         B = torch.tensor(B)
         return A, B
-
+    
     def make_bonds(self, ff, uqbondatomtypes):
         return torch.tensor([ff.get_bond(*at) for at in uqbondatomtypes])
 
