93185523807a3a8e6f9b1a2b39d7480344653ae4
Antonio Mirarchi <antonio.mirarchi@upf.edu>
2023-07-14 12:14:37 +0200

    clean code

--- torchmd/npzMol.py
+++ torchmd/npzMol.py
@@ -10,17 +10,17 @@ class npzMolecule:
         are also stored in the class but are not necessary for the simulation."""
 
         self.data = np.load(file)
-        self.z = torch.from_numpy(self.data["z"])
-        self.coords = torch.from_numpy(self.data["coord"])[:, :, None]
+        self.z = np.array(self.data["z"])
+        self.coords = np.array(self.data["coord"])[:, :, None]
         self.numAtoms = len(self.z)
-        self.embedding = self.z.tolist()
+        self.embedding = self.z.copy()
 
         self.masses = torch.tensor(
-            [periodictable_by_number[int(el)].mass for el in self.z.tolist()]
+            [periodictable_by_number[int(el)].mass for el in self.z]
         ).to(torch.float32)[:, None]
 
         self.element = np.array(
-            [periodictable_by_number[int(el)].symbol for el in self.z.tolist()]
+            [periodictable_by_number[int(el)].symbol for el in self.z]
         ).astype("object")
         self.atomtype = self.element.copy()
 
